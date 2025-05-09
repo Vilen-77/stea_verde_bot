3a4e944234d2e748b9d27be3b19fb14a1038aeef
Vilen-77 <5741400@gmail.com>
2025-05-09 16:19:52 +0300

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Привет! Я — Semantica Bot 🤖. Напиши /semantics <тема>, чтобы сгенерировать ключевые фразы."
-    )
-
-handler = CommandHandler("start", start_command)
